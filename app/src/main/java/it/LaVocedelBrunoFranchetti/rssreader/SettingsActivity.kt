83add2583ce0a3188cf6c1da75244d6e014ee915
Emilio Dalla Torre <emiliodallatorre12@live.com>
2018-08-26 13:26:59 +0200

    Succesfully created settings screen.

--- app/src/main/java/it/LaVocedelBrunoFranchetti/rssreader/SettingsActivity.kt
+++ app/src/main/java/it/LaVocedelBrunoFranchetti/rssreader/SettingsActivity.kt
@@ -2,11 +2,11 @@ package it.LaVocedelBrunoFranchetti.rssreader
 
 import android.os.Bundle
 import android.preference.PreferenceActivity
-import it.LaVocedelBrunoFranchetti.rssreader.R.layout.activity_settings
 
 class SettingsActivity: PreferenceActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_settings)
+        // TODO: Do this with a fragment.
+        addPreferencesFromResource(R.xml.app_settings)
     }
 }
