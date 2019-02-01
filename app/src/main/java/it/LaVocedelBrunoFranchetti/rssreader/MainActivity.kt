8f407389808525c132093e8336124feccb4503d1
Emilio Dalla Torre <emiliodallatorre12@live.com>
2019-02-01 18:20:22 +0100

    Got a "working" release.

--- app/src/main/java/it/LaVocedelBrunoFranchetti/rssreader/MainActivity.kt
+++ app/src/main/java/it/LaVocedelBrunoFranchetti/rssreader/MainActivity.kt
@@ -5,24 +5,23 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import com.google.android.material.navigation.NavigationView
 import com.mikepenz.aboutlibraries.Libs
 import com.mikepenz.aboutlibraries.LibsBuilder
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
+    @Suppress("REDUNDANT_LABEL_WARNING")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)*/
         setContentView(R.layout.activity_main)
+
 
         setSupportActionBar(toolbar)
 
@@ -35,16 +34,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             startActivity(Intent.createChooser(emailIntent, "Invia con..."))
         }
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
-        swiper.setOnRefreshListener {
+        firstRefresh@GlobalScope.launch {
+            swiper.isRefreshing = true
             val feedRefresh = ParseFeed(this@MainActivity)
 
             GlobalScope.launch {
+                feedRefresh.execute().get()
+                swiper.isRefreshing = false
+            }
+        }
+
+        swiper.setOnRefreshListener {
+            val feedRefresh = ParseFeed(this@MainActivity)
+
+            swipeRefresh@GlobalScope.launch {
                 feedRefresh.execute().get()
                 swiper.isRefreshing = false
             }
@@ -53,22 +56,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     override fun onStart() {
         super.onStart()
-
-        swiper.isRefreshing = true
-        val feedRefresh = ParseFeed(this@MainActivity)
-
-        GlobalScope.launch {
-            feedRefresh.execute().get()
-            swiper.isRefreshing = false
-        }
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -82,7 +69,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            // R.id.action_settings -> true
             R.id.action_about -> {
                 LibsBuilder()
                         .withAboutIconShown(true)
@@ -94,32 +81,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
